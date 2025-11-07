4b35142b193f3df3ebba9648d3b2e161fb174016
goatbot-ai <hexonazad@gmail.com>
2025-11-07 17:05:59 +0600

    Create link.js

---
+++ link.js
@@ -0,0 +1,12 @@
+[
+  { "boys": ["https://files.catbox.moe/963wd0.jpg"], "girls": ["https://files.catbox.moe/i1bph2.jpg"] },
+  { "boys": ["https://files.catbox.moe/tk2pj8.jpg"], "girls": ["https://files.catbox.moe/ve2kkr.jpg"] },
+  { "boys": ["https://files.catbox.moe/boy3.png"], "girls": ["https://files.catbox.moe/girl3.png"] },
+  { "boys": ["https://files.catbox.moe/boy4.png"], "girls": ["https://files.catbox.moe/girl4.png"] },
+  { "boys": ["https://files.catbox.moe/boy5.png"], "girls": ["https://files.catbox.moe/girl5.png"] },
+  { "boys": ["https://files.catbox.moe/boy6.png"], "girls": ["https://files.catbox.moe/girl6.png"] },
+  { "boys": ["https://files.catbox.moe/boy7.png"], "girls": ["https://files.catbox.moe/girl7.png"] },
+  { "boys": ["https://files.catbox.moe/boy8.png"], "girls": ["https://files.catbox.moe/girl8.png"] },
+  { "boys": ["https://files.catbox.moe/boy9.png"], "girls": ["https://files.catbox.moe/girl9.png"] },
+  { "boys": ["https://files.catbox.moe/boy10.png"], "girls": ["https://files.catbox.moe/girl10.png"] }
+]
